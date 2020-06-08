d495b7b41c7df81e5d3bd4d59169f94cfb0d6523
andrerrh <andre.rodrigues.he@gmail.com>
2020-06-08 20:15:11 -0300

    v1.2

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,116 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import api from './api';
+import Header from './header';
+import { 
+    Container, 
+    Table, 
+    TableRow, 
+    TableCell, 
+    Dialog, 
+    Button, 
+    DialogTitle, 
+    DialogContent, 
+    DialogContentText, 
+    TextField, 
+    DialogActions} from '@material-ui/core';
+import './style.css';
 
 function App() {
-  return (
-    <p>Text</p>
-  );
+
+    const [ lista, setLista ] = useState([]); // imutabilidade
+    const [ open, setOpen ] = useState(false);
+    const [ modelo, setModelo ] = useState('');
+
+    function loadData() { 
+        api.get('/modelo').then((response) => { 
+            const itens = response.data;
+            setLista(itens);
+        });
+    }
+
+    useEffect(() => loadData(), [])
+
+    const openModal = () => setOpen(true);
+
+    // function closeModal() { setOpen(false); }
+    const closeModal = () => setOpen(false);
+
+     function addModelo() { 
+         const name = modelo;
+         api.post('/modelo', { name: name }).then((response) => {
+            setModelo('');
+            setOpen(false);
+            loadData();
+        })
+     }
+
+     function markAsDone(id) { 
+         api.patch(`/modelo/${id}/done`).then((response) => {
+             loadData()
+         })
+     }
+
+     function deleteModelo(id) {
+         api.delete(`/modelo/${id}`).then((response) => { 
+            loadData()
+         })
+     }
+    
+
+    return (
+        <>
+        <Header />
+        <Container maxWidth="lg" className="container"> 
+            <Table>
+                {lista.map(item => (
+                    <TableRow key={item.id}>
+                        <TableCell>{item.id}</TableCell>
+                        <TableCell>{item.name}</TableCell>
+                        <TableCell>
+                            <input type="checkbox" checked={item.done} onChange={() => markAsDone(item.id)}/>
+                        </TableCell>
+                        <TableCell>
+                            <Button variant="outlined" size="small" color="secondary" onClick={() => deleteModelo(item.id)}>Apagar</Button>
+                        </TableCell>
+                    </TableRow>
+                ))}
+            </Table>
+            <Button 
+                onClick={openModal}
+                variant="contained" 
+                color="primary" 
+                style={{marginTop: '20px'}}>
+                Adicionar
+            </Button>
+        </Container>
+        <Dialog open={open} onClose={closeModal} fullWidth={true} maxWidth="sm">
+            <DialogTitle id="form-dialog-title">Novo Modelo</DialogTitle>
+            <DialogContent>
+                <DialogContentText>
+                    Digite o modelo que pretende adicionar.
+                </DialogContentText>
+                <TextField
+                    autoFocus
+                    margin="dense"
+                    id="name"
+                    label="Modelo"
+                    type="email"
+                    fullWidth
+                    value={modelo}
+                    onChange={e => setModelo(e.target.value)}
+                />
+            </DialogContent>
+            <DialogActions>
+                <Button onClick={closeModal} color="primary">
+                    Cancelar
+                </Button>
+                <Button onClick={addModelo} color="primary">
+                    Salvar
+                </Button>
+            </DialogActions>
+        </Dialog>
+        </>
+    );
 }
 
 export default App;
