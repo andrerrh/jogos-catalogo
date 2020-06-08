1ff246b983ccd8f84f74ca67363440cfea20f7d5
andrerrh <andre.rodrigues.he@gmail.com>
2020-06-08 20:55:02 -0300

    v1.3

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import React, { useState, useEffect } from 'react';
-import Header from './header';
 import api from './api';
+import Header from './header';
 import { 
     Container, 
     Table, 
