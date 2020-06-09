b0eef9b2441648bd03151278a54f9ad7b3856b87
andrerrh <andre.rodrigues.he@gmail.com>
2020-06-09 01:30:06 -0300

    v1.4

--- src/api.js
+++ src/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-    baseURL: 'https://listatarefas-backend.herokuapp.com'
+    baseURL: 'https://catalogo-games.herokuapp.com/'
 })
 
 export default api;
